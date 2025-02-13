0f8bd599dbc765304a77a586ddc0c118f24a44c7
NeganSally <nighen.salatan@gmail.com>
2025-02-13 13:24:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+window.alert("Hello, world!"); //using window alert//
+
+document.write("Hihi"); //using document write//
+
+var A = "Beginning of string"; //Start of string//
+var B = ", End of string"; //End of string//
+
+document.write(A + B); //Will say "Beginning of string, end of string"//
+
+document.write(1+2); //Will add 1 and 2 (3)//
